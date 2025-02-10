25989d34e49b0bcce53494e8f24edafb91b6b18b
PopovAndriiCGS <andrii.pop@cgsteam.io>
2025-02-10 13:45:16 +0200

    Replace absolute mapping with relative for Prisma

--- backend/src/kanban/kanban.module.ts
+++ backend/src/kanban/kanban.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { KanbanController } from './kanban.controller';
 import { KanbanService } from './kanban.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [KanbanController],
